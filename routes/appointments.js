65a4a6f41a79dfd3c0c7a323a5d910a1421d2287
JuanXCV <jaconesab@agrodolores.es>
2018-10-24 12:16:16 +0200

    separo auth de client y profesional

--- routes/appointments.js
+++ routes/appointments.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const Date = require('../models/date')
+const Appointment = require('../models/appointment')
+const Professional = require('../models/professional')
+const Client = require('../models/client')
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Types.ObjectId;
 
 /* GET users listing. */
 router.get('/', (req, res, next) => {
   res.send('respond with a resource');
 });
 
+router.post('/:id', (req, res, next) => {
+  const ClientId = req.session.currentUser._id;
+  const {dateId} = req.body
+  const professionalId = req.params.id
+
+  const newAppointment = new Appointment({ professional: ObjectId(professionalId), date: ObjectId(dateId), client: ObjectId(ClientId) })
+
+  newAppointment.save()
+    .then(result => {
+      res.redirect(`/profile`);
+    })
+    .catch(next)
+});
 module.exports = router;
--- routes/professionals.js
+++ routes/professionals.js
@@ -2,8 +2,96 @@ const express = require('express');
 const router = express.Router();
 const Professional = require('../models/professional')
 const Date = require('../models/date')
+const middlewares = require('../middlewares/middlewares')
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 /* GET users listing. */
+
+router.get('/signup',middlewares.requireAnon ,(req, res, next) => {
+  res.render('signupprof')
+})
+
+
+
+
+router.post('/signup', middlewares.requireAnon, (req, res, next) => {
+  const {username, password, email, code} = req.body;
+
+  if ( !username || !password || !email ) {
+    req.flash('error', 'all fields are required')
+    return res.redirect('/signup')
+  }
+
+  Professional.findOne( {username} )
+  .then(result => {
+    if(result) {
+      req.flash('error','username is already exist');
+      return res.redirect('/signup');
+    }
+    Professional.findOne( {email} )
+    .then(result => {
+      if(result) {
+        req.flash('error','email is already exist');
+        return res.redirect('/signup');
+      }
+    })
+    .catch(next)
+
+    const salt  = bcrypt.genSaltSync(saltRounds);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+    const newUser = new Professional({ username, password: hashedPassword, email, role: "professional", code});
+
+    newUser.save()
+    .then(result => {
+      // guardamos el usuario en la session
+      req.session.currentUser = newUser;
+      // redirect siempre con barra
+      res.redirect(`/profile`);
+    })
+    .catch(next)
+    
+  })
+  .catch(next)
+});
+
+
+router.get('/login',middlewares.requireAnon ,(req, res, next) => {
+  res.render('loginprof')
+})
+
+router.post('/login', middlewares.requireAnon, (req, res, next) => {
+  const { username, password} = req.body;
+
+  if ( !username || !password ) {
+    req.flash('error', 'Username or password are incorrect');
+    return res.redirect('/login');
+  }
+  
+  Professional.findOne({username})
+  .then(user => {
+    if(!user) {
+      req.flash('error', 'Username or password are incorrect');
+      return res.redirect('/login');
+    }
+
+    if (bcrypt.compareSync(password /* provided password */, user.password/* hashed password */)) {
+      // Success
+      // Adding user in session
+      req.session.currentUser = user;
+      res.redirect(`/profile`);
+    } else {
+      // No, no, no, no, prohibido
+      req.flash('error', 'Username or password are incorrect');
+      res.redirect('/login');
+    }
+  })
+  .catch(next)
+})
+
+
+
+
 router.get('/:id', (req, res, next) => {
   const id = req.params.id
 
