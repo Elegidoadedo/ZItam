d34b56721a5b68b2ec92a61d63c206876a8f85ed
elegidoadedo <jmoralesmnz@gmail.com>
2018-10-24 12:16:11 +0200

    half donde form

--- routes/professionals.js
+++ routes/professionals.js
@@ -5,7 +5,7 @@ const Date = require('../models/date')
 
 /* GET users listing. */
 router.get('/:id', (req, res, next) => {
-  const id = req.params.id
+  const id = req.params.id;
 
   Professional.findById(id)
   .populate('timeBlock.date')
@@ -15,5 +15,38 @@ router.get('/:id', (req, res, next) => {
   .catch(next)
 
 });
+// First stage in the form service selector
+
+router.get('/:id', (req, res, next) => {
+  const id = req.params.id;
+  const service = req.params.service;
+
+  Professional.find({id, service})
+  .populate('timeBlock.date')
+  .then(professional => {
+    res.render (`/$(id)/$(service)/employees`, {professional})
+  })
+  .catch(next)
+
+});
+ 
+
+// Second stage in the form employee selector
+router.get(`/$(id)/$(service)/employees`, (req, res, next) => {
+  const id = req.params.id;
+  const service = req.params.service;
+  const employee = req.params.employee;
+
+  Professional.find({id, service, employee})
+  .populate('timeBlock.date')
+  .then(professional => {
+    res.render (`/$(id)/$(service)/employees/timeblock`, {professional})
+  })
+  .catch(next)
+
+});
+
+// Final Step in form, show timeblocking
+
 
 module.exports = router;
